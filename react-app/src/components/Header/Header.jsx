e2b48f69d8abd82761b6cb9095560e7c17c1274c
adski13 <71898448+adski13@users.noreply.github.com>
2023-05-18 10:46:41 +0300

    Header_fixed

--- react-app/src/components/Header/Header.jsx
+++ react-app/src/components/Header/Header.jsx
@@ -1,27 +1,23 @@
+//import Search from '../Search/Search'
 import styles from'./Header.module.css'
 import {Link} from 'react-router-dom'
 
 function Header(props)
 {
-
     const menu = [
         {title:'sdf',
-        url:'fds'},
-        {title:'sdd',
-        url:'fs'}
+        url:'../fds'},
+        {title:'Заказ',
+        url:'../order'}
     ]
 
     return(
         <div className={styles.header}>
-           
-                <Link className={styles.header_logo} to={props.title}>{props.title}</Link>
-            
-            
+            <Link className={styles.header_logo} to='/'>{props.title}</Link>
             <div className={styles.header_menu}>
                 {menu.map((item,idx)=>(
                     <Link key={`menu item ${idx}`} to={item.url}>{item.title}</Link>
                 ))}
-                
             </div>
         </div>
     )
