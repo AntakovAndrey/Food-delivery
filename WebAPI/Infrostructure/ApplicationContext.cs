a5cb2f2e7db0b79288fec0932987d3170bf708d8
adski13 <71898448+adski13@users.noreply.github.com>
2023-06-01 13:01:06 +0300

    ApplicationContextClassAdded

---
+++ WebAPI/Infrostructure/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Infrostructure
+{
+	public class ApplicationContext:DbContext
+	{
+		
+		public ApplicationContext() 
+		{
+			Database.EnsureDeleted();
+			Database.EnsureCreated();
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+		}
+	}
+}
