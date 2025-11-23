edf46dd6e073678380ea8436bdd0817922663db8
AdreyAntakov <andrey.antakov05@gmail.com>
2025-11-23 23:45:29 +0300

    Added swagger

--- Backend/IdentityService/Program.cs
+++ Backend/IdentityService/Program.cs
@@ -2,11 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
