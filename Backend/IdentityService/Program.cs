5bdaf8bd001d95380294c34f3e04ed1c26f40d1d
AdreyAntakov <andrey.antakov05@gmail.com>
2025-11-24 13:36:34 +0300

    Added database context to DI

--- Backend/IdentityService/Program.cs
+++ Backend/IdentityService/Program.cs
@@ -1,4 +1,16 @@
+using IdentityService.Infrastructure.Extensions;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new Exception("No connection string found");
+}
+
+builder.Services.AddDatabaseContext(connectionString);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
