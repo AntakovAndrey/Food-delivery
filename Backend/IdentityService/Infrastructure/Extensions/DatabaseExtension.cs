eab559ed8a355c2a72322302ba22edecc36aac1c
AdreyAntakov <andrey.antakov05@gmail.com>
2025-11-24 13:35:30 +0300

    Infrastructure extensions added

---
+++ Backend/IdentityService/Infrastructure/Extensions/DatabaseExtension.cs
@@ -0,0 +1,15 @@
+﻿using IdentityService.Domain.Interfaces;
+using IdentityService.Infrastructure.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityService.Infrastructure.Extensions;
+
+public static class DatabaseExtension
+{
+    public static IServiceCollection AddDatabaseContext(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<IAppDbContext, AppDbContext>(options=>
+            options.UseNpgsql(connectionString));
+        return services;
+    }
+}
---
+++ Backend/IdentityService/Infrastructure/Extensions/RepositoriesExtension.cs
@@ -0,0 +1,16 @@
+﻿using IdentityService.Domain.Interfaces;
+using IdentityService.Infrastructure.Repositories;
+
+namespace IdentityService.Infrastructure.Extensions;
+
+public static class RepositoriesExtension
+{
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddTransient<IUserRepository, UserRepository>();
+        services.AddTransient<IAddressRepository, AddressRepository>();
+        services.AddTransient<IRefreshTokenRepository, RefreshTokeRepository>();
+        services.AddTransient<IRoleRepository, RoleRepository>();
+        return services;
+    }
+}
